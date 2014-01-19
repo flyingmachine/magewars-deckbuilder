d3aa08e51c2c451c8a26701f2b8e8947358b430a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-19 15:35:12 -0500

    baby steps

--- src/browser/magewars_deckbuilder/deck.cljs
+++ src/browser/magewars_deckbuilder/deck.cljs
@@ -1,7 +1,19 @@
 (ns magewars-deckbuilder.deck
   (:require-macros
-    [tailrecursion.javelin :refer [defc defc= cell=]])
+   [tailrecursion.javelin :refer [defc defc= cell=]])
   (:require
    [tailrecursion.javelin :as j :refer [cell]]))
 
-(defc deck ["test"])
+
+(def attributes
+  {:schools [:chaos :divine :nature]
+   :types [:creature :spell]})
+
+(defc deck [{:title "minotaur"
+             :school :chaos
+             :cost 12
+             :type :creature}
+            {:title "heal"
+             :school :divine
+             :cost 3
+             :type :spell}])
