80b465b06a50b6f6a0146377c9b368e63f2f6944
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-19 10:08:34 -0500

    initial commit

---
+++ src/browser/magewars_deckbuilder/deck.cljs
@@ -0,0 +1,7 @@
+(ns magewars-deckbuilder.deck
+  (:require-macros
+    [tailrecursion.javelin :refer [defc defc= cell=]])
+  (:require
+   [tailrecursion.javelin :as j :refer [cell]]))
+
+(defc deck ["test"])
