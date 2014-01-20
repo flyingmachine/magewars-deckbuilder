93e1f518c7bd6140a72c1cfeaf2eb52c50e249f3
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-20 13:18:25 -0500

    add a 'base set'

--- src/browser/magewars_deckbuilder/deck.cljs
+++ src/browser/magewars_deckbuilder/deck.cljs
@@ -6,14 +6,33 @@
 
 
 (def attributes
-  {:school #{:chaos :divine :nature}
-   :type #{:creature :spell}})
+  {:school #{:chaos :divine :nature :elemental}
+   :type #{:creature :spell :equipment}})
 
-(defc deck [{:title "minotaur"
-             :school :chaos
-             :cost 12
-             :type :creature}
-            {:title "heal"
-             :school :divine
-             :cost 3
-             :type :spell}])
+(def deck
+  #{{:title "minotaur"
+     :school :chaos
+     :cost 12
+     :type :creature}
+    {:title "heal"
+     :school :divine
+     :cost 3
+     :type :spell}})
+
+(def base-set
+  #{{:title "magic wand"
+     :school :elemental
+     :cost 8
+     :type :equipment}
+    {:title "fox"
+     :school :nature
+     :cost 3
+     :type :creature}
+    {:title "panther"
+     :school :nature
+     :cost 12
+     :type :creature}
+    {:title "fireball"
+     :school :chaos
+     :cost 6
+     :type :spell}})
