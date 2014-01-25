b88906406d7afa1090ed59109d0b4ff661fc02aa
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-25 10:24:22 -0500

    fixes

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject
+ magewars-deckbuilder
+ "0.1.0-SNAPSHOT"
+ :dependencies
+ [[tailrecursion/boot.core "1.2.3"]
+  [tailrecursion/boot.task "1.0.2"]
+  [tailrecursion/hoplon "4.0.10"]
+  [org.clojure/clojurescript "0.0-2138"]
+  [org.clojure/tools.nrepl "0.2.3"]]
+ :source-paths
+ ["src/server" "src/browser"])
--- src/server/magewars_deckbuilder/cards.clj
+++ src/server/magewars_deckbuilder/cards.clj
@@ -1,2 +1,69 @@
-(ns magewars-deckbuilder.cards)
+(ns magewars-deckbuilder.cards
+  (:require [clojure.java.io :as io]))
 
+(defn slurp-resource
+  [path]
+  (-> path
+      io/resource
+      slurp))
+
+(defn read-resource
+  [path]
+  (-> path
+      slurp-resource
+      read-string))
+
+(defn read-edn
+  [& pieces]
+  (->> pieces
+       reverse
+       (map name)
+       (into [".edn"])
+       reverse
+       (clojure.string/join "")
+       read-resource))
+
+;; "unpack" cards
+(def common-attributes
+  {:attacks      {:type :attack
+                  :speed :quick}
+   :conjurations {:type :conjuration
+                  :targets #{:zone}
+                  :speed :quick
+                  :range [0 1]}
+   :creatures    {:type :creature
+                  :targets #{:zone}
+                  :speed :slow}
+   :enchantments {:type :enchantment
+                  :speed :quick}
+   :equipment    {:type :equipment
+                  :targets #{:mage}
+                  :speed :quick
+                  :range [0 2]}
+   :incantations {:type :incantation}})
+
+(def transformations
+  {:attacks [(fn [x]
+               (let [attack-keys [:dice :damage-type :effects :traits]]
+                 (merge (apply dissoc x attack-keys)
+                        {:attacks #{(select-keys x (conj attack-keys :range))}})))]})
+
+;;
+(defn card-set
+  [set-name]
+  (read-edn "sets/" set-name))
+
+(defn cards-by-type
+  [card-type common-attributes transformations sets]
+  (->> (read-edn "cards/" card-type)
+       (map (fn [x]
+              (let [card (merge (card-type common-attributes) x)]
+                (if-let [t (card-type transformations)]
+                  ((apply comp t) card)
+                  card))))
+       (into #{})))
+
+(def cards
+  (->> (keys common)
+       (map #(cards-by-type % common-attributes transformations []))
+       (reduce into)))
